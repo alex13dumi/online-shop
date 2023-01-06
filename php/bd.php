57397c326b51705c252d0ac1867cf171af8772a9
alex13dumi <alex13.dumi@gmail.com>
2023-01-06 16:13:09 +0200

    removed Update;

--- php/bd.php
+++ php/bd.php
@@ -12,32 +12,32 @@
 </head>
 
 <body>
-<?php
-// define variables and set to empty values
-$nameErr = $emailErr = $phoneErr = "";
-$name = $phone = $email = "";
-if (empty($name))
-{
-    $nameErr = "Name is required";
-}
-else
-{
-    if (!preg_match("/^[a-zA-Z-' ]*$/", $name))
+    <?php
+    // define variables and set to empty values
+    $nameErr = $emailErr = $phoneErr = "";
+    $name = $phone = $email = "";
+    if (empty($name))
     {
-        $nameErr = "Only characters allowed !";
+        $nameErr = "Name is required";
     }
-}
-if (empty($email))
-{
-    $emailErr = "Email is required";
-}
-else
-{
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+    else
     {
-        $emailErr = "Invalid email format";
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name))
+        {
+            $nameErr = "Only characters allowed !";
+        }
     }
-}
+    if (empty($email))
+    {
+        $emailErr = "Email is required";
+    }
+    else
+    {
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        {
+            $emailErr = "Invalid email format";
+        }
+    }
 ?>
 
 <hr>
@@ -86,254 +86,171 @@ else
 </form>
 <hr>
 
-<hr>
-<p style="text-align: center;"><b>Update client</p>
-<form method="post" autocomplete="off">
-    <p>
-        <label for="update_name"><b>Nume complet:</label>
-        <input type="text" name="update_name" id="update_name" style="width: 250px;">
-        <br><br>
-        <label for="update_phone"><b>Nr. telefon:</label>
-        <input type="text" name="update_phone" id="update_phone" style="width: 250px;">
-        <br><br>
-        <label for="update_email"><b>E-mail:</label>
-        <input type="text" name="update_email" id="update_email" style="width: 250px;">
-        <br><br>
-    </p>
-    <input type="submit" name="UpdateClient" value="Cauta">
-</form>
-<hr>
-
-
-
-<?php
-function FormatName($name)
-{
-    $name = trim($name);
-    $name = strtolower($name);
-    $name = ucwords($name);
-    return $name;
-}
-function CreateClient()
-{
-    global $name, $phone, $email;
-    $name=FormatName($_POST['create_client']);
-    $phone=trim($_POST['create_phone']);
-    $email=trim($_POST['create_email']);
-
-    try
+    <?php
+    function FormatName($name)
     {
-        if(empty($name) or empty($phone) or empty($email))
-        {
-            throw new Exception('Campul nu trebuie sa fie gol! Va rugam completati cu ceva');
-        }
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $name))
-        {
-            throw new Exception('Only letters and white space allowed');
-        }
-        if (!preg_match("/^(?:(?:(?:00\s?|\+)40\s?|0)(?:7\d{2}\s?\d{3}\s?\d{3}|(21|31)\d{1}\s?\d{3}\s?\d{3}|((2|3)[3-7]\d{1})\s?\d{3}\s?\d{3}|(8|9)0\d{1}\s?\d{3}\s?\d{3}))$/", $phone))
-        {
-            throw new Exception('Only 10 digit numbers are allowed ! ');
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-        {
-            throw new Exception('Invalid email format');
-        }
-
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
-
-        $sql='INSERT INTO `tblClienti` (`numeClient`, `telefonClient`, `emailClient`) VALUES (?,?,?)';
-        $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
-
-        if(!is_null($mysqli))
-        {
-            echo 'Success.....' .$mysqli->host_info;
-            echo '<br></br>';
-            echo 'Connected !\nClient library version: ' .$mysqli->client_info;
-            echo '<br ></br >';
-            echo 'Server' .$mysqli->server_info;
-        }
-        else
-        {
-            echo "\nCouldn\'t connect to $mysqli->host_info\n";
-        }
-
-        $stmt=$mysqli->prepare($sql);
-        $stmt->execute([$name,$phone,$email]);
-
-        if(!$stmt->affected_rows)
-        {
-            throw new Exception('Couldn\'t INSERT into `tblClienti` !');
-        }
-        echo 'Inserted user: ', $name, ' ', $phone,' ', $email;
-        $stmt->close();
-        $mysqli->close();
-
-    } catch(Exception $e){
-        echo 'Caught exception: ', $e->getMessage(), "\n";
-        exit();
+        $name = trim($name);
+        $name = strtolower($name);
+        $name = ucwords($name);
+        return $name;
     }
-}
-
-function DeleteClient()
-{
-    try
+    function CreateClient()
     {
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+        global $name, $phone, $email;
+        $name=FormatName($_POST['create_client']);
+        $phone=trim($_POST['create_phone']);
+        $email=trim($_POST['create_email']);
 
-        $sql = 'DELETE FROM `tblClienti` WHERE `numeClient`=\''.FormatName($_POST['delete_client']).'\'';
-        $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
-
-        if(!is_null($mysqli))
+        try
         {
-            echo 'Success.....' .$mysqli->host_info;
-            echo '<br></br>';
-            echo 'Connected !\nClient library version: ' .$mysqli->client_info;
-            echo '<br ></br >';
-            echo 'Server' .$mysqli->server_info;
-        }
-        else
-        {
-            echo "\nCouldn\'t connect to $mysqli->host_info\n";
-        }
-
-        $stmt=$mysqli->prepare($sql);
-        $stmt->execute();
-
-        if(!$stmt->affected_rows)
-        {
-            throw new Exception('Username doesn\'t belong to this DB !');
-        }
-        echo 'Deleted user: ', FormatName($_POST['delete_client']);
-        $stmt->close();
-        $mysqli->close();
-    }catch(Exception $e){
-        echo '<br></br>';
-        echo'Caught exception: ' .$e->getMessage();
-        exit();
-    }
-}
-
-function SearchClient()
-{
-    try
-    {
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
-
-        $sql='SELECT `numeClient`, `telefonClient`, `emailClient`, `pretComanda`, `adresaComanda`, `statusComanda`, `dataPlasareComanda` FROM `tblClienti` JOIN `tblComenzi`'.
-            ' ON `idClient`=`codClient` WHERE `numeClient`=\''.FormatName($_POST['search_client']).'\' AND `telefonClient`=\''.trim($_POST['search_phone']).'\'' ;
-        echo $sql;
-        echo '<br></br>';
-
-        //`numeClient`=\''.$_POST['search_client'].'\'
-        $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
-
-        if(!is_null($mysqli))
-        {
-            echo 'Success.....' .$mysqli->host_info;
-            echo '<br></br>';
-            echo 'Connected !\nClient library version: ' .$mysqli->client_info;
-            echo '<br ></br >';
-            echo 'Server' .$mysqli->server_info;
-        }
-        else
-        {
-            echo "\nCouldn\'t connect to $mysqli->host_info\n";
-        }
-
-        $result=$mysqli->query($sql);
-        if(!$result->num_rows)
-            throw new Exception('Name or telephone doesn\'t exist');
-        else
-        {
-            while ($obj = $result->fetch_object())
+            if(empty($name) or empty($phone) or empty($email))
             {
-                printf("%s %s %s %s %s %s %s\n", $obj->numeClient, $obj->telefonClient, $obj->emailClient, $obj->pretComanda, $obj->adresaComanda, $obj->statusComanda, $obj->dataPlasareComanda);
-                echo '<br></br>';
+                throw new Exception('Campul nu trebuie sa fie gol! Va rugam completati cu ceva');
             }
+            if (!preg_match("/^[a-zA-Z-' ]*$/", $name))
+            {
+                throw new Exception('Only letters and white space allowed');
+            }
+            if (!preg_match("/^(?:(?:(?:00\s?|\+)40\s?|0)(?:7\d{2}\s?\d{3}\s?\d{3}|(21|31)\d{1}\s?\d{3}\s?\d{3}|((2|3)[3-7]\d{1})\s?\d{3}\s?\d{3}|(8|9)0\d{1}\s?\d{3}\s?\d{3}))$/", $phone))
+            {
+                throw new Exception('Only 10 digit numbers are allowed ! ');
+            }
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+            {
+                throw new Exception('Invalid email format');
+            }
+
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+
+            $sql='INSERT INTO `tblClienti` (`numeClient`, `telefonClient`, `emailClient`) VALUES (?,?,?)';
+            $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
+
+            if(!is_null($mysqli))
+            {
+                echo 'Success.....' .$mysqli->host_info;
+                echo '<br></br>';
+                echo 'Connected !\nClient library version: ' .$mysqli->client_info;
+                echo '<br ></br >';
+                echo 'Server' .$mysqli->server_info;
+            }
+            else
+            {
+                echo "\nCouldn\'t connect to $mysqli->host_info\n";
+            }
+
+            $stmt=$mysqli->prepare($sql);
+            $stmt->execute([$name,$phone,$email]);
+
+            if(!$stmt->affected_rows)
+            {
+                throw new Exception('Couldn\'t INSERT into `tblClienti` !');
+            }
+            echo 'Inserted user: ', $name, ' ', $phone,' ', $email;
+            $stmt->close();
+            $mysqli->close();
+
+        } catch(Exception $e){
+            echo 'Caught exception: ', $e->getMessage(), "\n";
+            exit();
         }
-        $mysqli->close();
-    }catch(Exception $e){
-        echo '<br></br>';
-        echo'Caught exception: ' .$e->getMessage();
-        exit();
     }
-}
 
-function UpdateClient()
-{
-    global $name, $phone, $email;
-    $name=FormatName($_POST['update_client']);
-    $phone=trim($_POST['update_phone']);
-    $email=trim($_POST['update_email']);
-
-    try
+    function DeleteClient()
     {
-        if(empty($name) or empty($phone) or empty($email))
+        try
         {
-            throw new Exception('Field empty !');
-        }
-        if (!preg_match("/^[a-zA-Z-' ]*$/", $name))
-        {
-            throw new Exception('Only letters and white space allowed');
-        }
-        if (!preg_match("/^(?:(?:(?:00\s?|\+)40\s?|0)(?:7\d{2}\s?\d{3}\s?\d{3}|(21|31)\d{1}\s?\d{3}\s?\d{3}|((2|3)[3-7]\d{1})\s?\d{3}\s?\d{3}|(8|9)0\d{1}\s?\d{3}\s?\d{3}))$/", $phone))
-        {
-            throw new Exception('Only 10 digit numbers are allowed ! ');
-        }
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-        {
-            throw new Exception('Invalid email format');
-        }
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
 
-        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+            $sql = 'DELETE FROM `tblClienti` WHERE `numeClient`=\''.FormatName($_POST['delete_client']).'\'';
+            $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
 
-        $sql='UPDATE `tblClienti` SET `numeClient`=?, `telefonClient`=?, `emailClient`=?';
-        $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
+            if(!is_null($mysqli))
+            {
+                echo 'Success.....' .$mysqli->host_info;
+                echo '<br></br>';
+                echo 'Connected !\nClient library version: ' .$mysqli->client_info;
+                echo '<br ></br >';
+                echo 'Server' .$mysqli->server_info;
+            }
+            else
+            {
+                echo "\nCouldn\'t connect to $mysqli->host_info\n";
+            }
 
-        if(!is_null($mysqli))
-        {
-            echo 'Success.....' .$mysqli->host_info;
+            $stmt=$mysqli->prepare($sql);
+            $stmt->execute();
+
+            if(!$stmt->affected_rows)
+            {
+                throw new Exception('Username doesn\'t belong to this DB !');
+            }
+            echo 'Deleted user: ', FormatName($_POST['delete_client']);
+            $stmt->close();
+            $mysqli->close();
+        }catch(Exception $e){
             echo '<br></br>';
-            echo 'Connected !\nClient library version: ' .$mysqli->client_info;
-            echo '<br ></br >';
-            echo 'Server' .$mysqli->server_info;
+            echo'Caught exception: ' .$e->getMessage();
+            exit();
         }
-        else
-        {
-            echo "\nCouldn\'t connect to $mysqli->host_info\n";
-        }
-
-        $stmt=$mysqli->prepare($sql);
-        $stmt->execute([$name,$phone,$email]);
-
-        if(!$stmt->affected_rows)
-        {
-            throw new Exception('Couldn\'t INSERT into `tblClienti` !');
-        }
-        echo 'Updated user: ', $name, ' ', $phone,' ', $email;
-        $stmt->close();
-        $mysqli->close();
-
-    } catch(Exception $e){
-        echo 'Caught exception: ', $e->getMessage(), "\n";
-        exit();
     }
-}
 
-if (isset($_POST['CreateClient'])){
-    CreateClient();
-}
-if (isset($_POST['DeleteClient'])){
-    DeleteClient();
-}
-if(isset($_POST['SearchClient'])){
-    SearchClient();
-}
-if(isset($_POST['UpdateClient'])){
-    UpdateClient();
-}
-?>
+    function SearchClient()
+    {
+        try
+        {
+            mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+
+            $sql='SELECT `numeClient`, `telefonClient`, `emailClient`, `pretComanda`, `adresaComanda`, `statusComanda`, `dataPlasareComanda` FROM `tblClienti` JOIN `tblComenzi`'.
+                ' ON `idClient`=`codClient` WHERE `numeClient`=\''.FormatName($_POST['search_client']).'\' AND `telefonClient`=\''.trim($_POST['search_phone']).'\'' ;
+            echo $sql;
+            echo '<br></br>';
+
+            //`numeClient`=\''.$_POST['search_client'].'\'
+            $mysqli=new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
+
+            if(!is_null($mysqli))
+            {
+                echo 'Success.....' .$mysqli->host_info;
+                echo '<br></br>';
+                echo 'Connected !\nClient library version: ' .$mysqli->client_info;
+                echo '<br ></br >';
+                echo 'Server' .$mysqli->server_info;
+            }
+            else
+            {
+                echo "\nCouldn\'t connect to $mysqli->host_info\n";
+            }
+
+            $result=$mysqli->query($sql);
+            if(!$result->num_rows)
+                throw new Exception('Name or telephone doesn\'t exist');
+            else
+            {
+                while ($obj = $result->fetch_object())
+                {
+                    printf("%s %s %s %s %s %s %s\n", $obj->numeClient, $obj->telefonClient, $obj->emailClient, $obj->pretComanda, $obj->adresaComanda, $obj->statusComanda, $obj->dataPlasareComanda);
+                    echo '<br></br>';
+                }
+            }
+            $mysqli->close();
+        }catch(Exception $e){
+            echo '<br></br>';
+            echo'Caught exception: ' .$e->getMessage();
+            exit();
+        }
+    }
+
+    if (isset($_POST['CreateClient'])){
+        CreateClient();
+    }
+    if (isset($_POST['DeleteClient'])){
+        DeleteClient();
+    }
+    if(isset($_POST['SearchClient'])){
+        SearchClient();
+    }
+
+    ?>
 
 </body>
 </html>

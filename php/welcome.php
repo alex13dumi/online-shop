6fe256fc1a88ba314c04360404d96338a91c6511
alex13dumi <alex13.dumi@gmail.com>
2023-01-07 20:28:42 +0200

    added new features to welcome.php: add a new client that can make an order. Search some command if some were made by you

--- php/welcome.php
+++ php/welcome.php
@@ -12,36 +12,175 @@
     function UpdateUser()
     {
         try {
-
-            if (empty($_POST['username_client_nou']))
-                throw new Exception('Username can\'t be empty');
-                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
-            $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-            $sql = 'UPDATE `users` SET `username`=\'' . trim($_POST['username_client_nou']) . '\' WHERE id=' . $_SESSION["id"];
-
-            if ($stmt = $mysqli->prepare($sql)) 
+            if($_SERVER["REQUEST_METHOD"] == "POST")
             {
-                if ($stmt->execute()) 
-                {
-                    session_destroy();
-                    header("location: login.php");
-                    exit();
-                } 
-                else 
-                {
-                    echo "Oops! Something went wrong. Please try again later.";
+                    if (empty($_POST['username_client_nou']))
+                        throw new Exception('Username can\'t be empty');
+
+                    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+                    $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+                    $sql = 'UPDATE `users` SET `username`=\'' . trim($_POST['username_client_nou']) . '\' WHERE id=' .$_SESSION["id"];
+                    if ($stmt = $mysqli->prepare($sql))
+                    {
+                        if ($stmt->execute())
+                        {
+                            session_destroy();
+                            header("location: login.php");
+                            exit();
+                        }
+                        else
+                        {
+                            echo "Oops! Something went wrong. Please try again later.";
+                        }
+                        $stmt->close();
+                    }
+                    $mysqli->close();
                 }
-                $stmt->close();
-            }
-            $mysqli->close();
         }catch (Exception $e)
         {
             echo '<br></br> Caught exception: '. $e->getMessage();
         }
     }
 
-    if(isset($_POST['UpdateUser'])){
+    function CreateClient()
+    {
+        try
+        {
+            if($_SERVER["REQUEST_METHOD"] == "POST")
+            {
+                    global $name, $phone, $email;
+                    $name = FormatName($_POST['create_client']);
+                    $phone = trim($_POST['create_phone']);
+                    $email = trim($_POST['create_email']);
+                    $coduser = $_SESSION["id"];
+
+                    if (empty($name) or empty($phone) or empty($email)) {
+                        throw new Exception('Campul nu trebuie sa fie gol! Va rugam completati cu ceva');
+                    }
+                    if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+                        throw new Exception('Only letters and white space allowed');
+                    }
+                    if (!preg_match("/^(?:(?:(?:00\s?|\+)40\s?|0)(?:7\d{2}\s?\d{3}\s?\d{3}|(21|31)\d{1}\s?\d{3}\s?\d{3}|((2|3)[3-7]\d{1})\s?\d{3}\s?\d{3}|(8|9)0\d{1}\s?\d{3}\s?\d{3}))$/", $phone)) {
+                        throw new Exception('Only 10 digit numbers are allowed ! ');
+                    }
+                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                        throw new Exception('Invalid email format');
+                    } else {
+                        mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+
+                        $sql = 'INSERT INTO `tblClienti` (`numeClient`, `telefonClient`, `emailClient`, `codUser`) VALUES (?,?,?,?)';
+                        $mysqli = new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
+                        echo $sql;
+                        if (!is_null($mysqli)) {
+                            echo 'Success.....' . $mysqli->host_info;
+                            echo '<br></br>';
+                            echo 'Connected ! Client library version: ' . $mysqli->client_info;
+                            echo '<br ></br >';
+                            echo 'Server' . $mysqli->server_info;
+                        } else {
+                            echo "\nCouldn\'t connect to $mysqli->host_info\n";
+                        }
+
+                        $stmt = $mysqli->prepare($sql);
+                        $stmt->execute([$name, $phone, $email, $coduser]);
+                        echo $coduser;
+
+                        if (!$stmt->affected_rows) {
+                            throw new Exception('Couldn\'t INSERT into `tblClienti` !');
+                        }
+                        echo 'Inserted user: ', $name, ' ', $phone, ' ', $email;
+                        $stmt->close();
+                        $mysqli->close();
+                    }
+
+                }
+            }catch (Exception $e) {
+                echo 'Caught exception: ', $e->getMessage(), "\n";
+                exit();
+        }
+    }
+
+    function SearchClient()
+    {
+        if($_SERVER["REQUEST_METHOD"] == "POST") {
+            try {
+                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
+
+                $sql = 'SELECT `numeClient`, `telefonClient`, `emailClient`, `pretComanda`, `adresaComanda`, `statusComanda`, `dataPlasareComanda` FROM `tblClienti` JOIN `tblComenzi`' .
+                       ' ON `codUser`=`codClient` WHERE `numeClient`=\'' . FormatName($_POST['search_client']) . '\' AND `telefonClient`=\'' . trim($_POST['search_phone']) . '\' AND `codUser`='.$_SESSION['id'].'';
+
+                //`numeClient`=\''.$_POST['search_client'].'\'
+                $mysqli = new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
+
+                if (!is_null($mysqli))
+                {
+                    echo 'Success.....' . $mysqli->host_info;
+                    echo '<br></br>';
+                    echo 'Connected !\nClient library version: ' . $mysqli->client_info;
+                    echo '<br ></br >';
+                    echo 'Server' . $mysqli->server_info;
+                } else {
+                    echo "\nCouldn\'t connect to $mysqli->host_info\n";
+                }
+
+                $result = $mysqli->query($sql);
+                if (!$result->num_rows)
+                    throw new Exception('Name or telephone doesn\'t exist or order doesn\'t belong to you !');
+                else
+                {
+                    $i=0;
+                    while ($obj = $result->fetch_object())
+                    {
+                        echo'<table class="table table-dark">
+                                  <thead class="thead-dark">
+                                    <tr>
+                                      <th scope="col">#</th>
+                                      <th scope="col">numeClient</th>
+                                      <th scope="col">telefonClient</th>
+                                      <th scope="col">emailClient</th>
+                                      <th scope="col">pretComanda</th>
+                                      <th scope="col">adresaComanda</th>
+                                      <th scope="col">statusComanda</th>
+                                      <th scope="col">dataPlasareComanda</th>
+                                    </tr>
+                                  </thead>
+                            ';
+                        echo'
+                                  <tbody>
+                                    <tr>
+                                      <th scope="row">' .$i. '</th>
+                                      <td>' .$obj->numeClient. '</td>
+                                      <td>' .$obj->telefonClient. '</td>
+                                      <td>' .$obj->emailClient. '</td>
+                                      <td>' .$obj->pretComanda. '</td>
+                                      <td>' .$obj->adresaComanda. '</td>
+                                      <td>' .$obj->statusComanda. '</td>
+                                      <td>' .$obj->dataPlasareComanda. '</td>
+                                    </tr>
+                                 </tbody>
+                                </table>
+                            ';
+                        $i++;
+                    }
+                }
+                $result->close();
+                $mysqli->close();
+            } catch (Exception $e) {
+                echo '<br></br>';
+                echo 'Caught exception: ' . $e->getMessage();
+                exit();
+            }
+        }
+    }
+
+    if(isset($_POST['CreateClient'])){
+        CreateClient();
+    }
+    if(isset($_POST['UpdateUser']) ){
         UpdateUser();
+    }
+    if(isset($_POST['SearchClient'])){
+        SearchClient();
     }
 ?>
 
@@ -73,12 +212,41 @@
             <input type="username" class="form-control" name="username_client" id="username_client" value="<?php echo $_SESSION['username'];?>" readonly>
             <label for="username_client"><b>New username:</label>
             <input type="username" class="form-control" name="username_client_nou" id="username_client_nou" >
+            <br>
+            <div class="text-center">
+                <input type="submit" name="UpdateUser" value="Update">
+            </div>
+            </br>
+            <p style="text-align: center;"><b>Adauga client</p>
+            <label for="nume"><b>Nume:</label>
+            <input type="username" class="form-control" name="create_client" id="create_client" >
+            <label for="nume"><b>Telefon:</label>
+            <input type="username" class="form-control" name="create_phone" id="create_phone" >
+            <label for="nume"><b>Email:</label>
+            <input type="username" class="form-control" name="create_email" id="create_email" >
+            <br>
+            <div class="text-center">
+                <input type="submit" name="CreateClient" value="Add">
+            </div>
+            </br>
+            <hr>
+            <p style="text-align: center;"><b>Cauta comanda</p>
+            <form method="post" autocomplete="off">
+                <p>
+                    <label for="search_client"><b>Nume complet:</label>
+                    <input type="text" name="search_client" id="search_client" style="width: 250px;">
+                    <br><br>
+                    <label for="search_phone"><b>Nr. telefon:</label>
+                    <input type="text" name="search_phone" id="search_phone" style="width: 250px;">
+                    <br><br>
+                </p>
+                <input type="submit" name="SearchClient" value="Cauta">
+            </form>
+            <hr>
         </div>
         <br>
         </p>
-        <div class="text-center">
-            <input type="submit" name="UpdateUser" value="Update">
-        </div>
+
     </form>
 </center>
 <hr>

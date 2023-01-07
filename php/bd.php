0015233dfa2497cf677a1e10fd4edc2df969b140
alex13dumi <alex13.dumi@gmail.com>
2023-01-07 20:26:39 +0200

    bd.php now has only delete functions

--- php/bd.php
+++ php/bd.php
@@ -41,25 +41,6 @@
 ?>
 
     <hr>
-    <p style="text-align: center;"><b>Adauga client</p>
-    <form method="post" autocomplete="off">
-        <p>
-            <label for="create_client"><b>Nume complet:</label>
-            <span class="error">* <?php echo $nameErr;?></span>
-            <input type="text" name="create_client" id="create_client" style="width: 250px;" value="<?php echo $GLOBALS['name'];?>">
-            <br><br>
-            <label for="create_phone"><b>Numar telefon:</label>
-            <input type="tel" id="create_phone" name="create_phone" value="<?php echo $GLOBALS['phone'];?>">
-            <br><br>
-            <label for="create_email"><b>Email:</label>
-            <span class="error">* <?php echo $emailErr;?></span>
-            <input type="text" name="create_email" id="create_email" style="width: 350px;" value="<?php echo $GLOBALS['email'];?>">
-            <br><br>
-        </p>
-        <input type="submit" name="CreateClient" value="Adauga">
-    </form>
-    <hr>
-    <hr>
     <p style="text-align: center;"><b>Sterge client</p>
     <form method="post" autocomplete="off">
         <p>
@@ -71,77 +52,11 @@
     </form>
     <hr>
     
-    <hr>
-    <p style="text-align: center;"><b>Cauta comanda</p>
-    <form method="post" autocomplete="off">
-        <p>
-            <label for="search_client"><b>Nume complet:</label>
-            <input type="text" name="search_client" id="search_client" style="width: 250px;">
-            <br><br>
-            <label for="search_phone"><b>Nr. telefon:</label>
-            <input type="text" name="search_phone" id="search_phone" style="width: 250px;">
-            <br><br>
-        </p>
-        <input type="submit" name="SearchClient" value="Cauta">
-    </form>
-    <hr>
+
 
 <?php
     require "functions.php";
-    function CreateClient()
-    {
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-            global $name, $phone, $email;
-            $name = FormatName($_POST['create_client']);
-            $phone = trim($_POST['create_phone']);
-            $email = trim($_POST['create_email']);
 
-            try {
-                if (empty($name) or empty($phone) or empty($email)) {
-                    throw new Exception('Campul nu trebuie sa fie gol! Va rugam completati cu ceva');
-                }
-                if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                    throw new Exception('Only letters and white space allowed');
-                }
-                if (!preg_match("/^(?:(?:(?:00\s?|\+)40\s?|0)(?:7\d{2}\s?\d{3}\s?\d{3}|(21|31)\d{1}\s?\d{3}\s?\d{3}|((2|3)[3-7]\d{1})\s?\d{3}\s?\d{3}|(8|9)0\d{1}\s?\d{3}\s?\d{3}))$/", $phone)) {
-                    throw new Exception('Only 10 digit numbers are allowed ! ');
-                }
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    throw new Exception('Invalid email format');
-                } else {
-                    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
-
-                    $sql = 'INSERT INTO `tblClienti` (`numeClient`, `telefonClient`, `emailClient`) VALUES (?,?,?)';
-                    $mysqli = new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
-
-                    if (!is_null($mysqli)) {
-                        echo 'Success.....' . $mysqli->host_info;
-                        echo '<br></br>';
-                        echo 'Connected !\nClient library version: ' . $mysqli->client_info;
-                        echo '<br ></br >';
-                        echo 'Server' . $mysqli->server_info;
-                    } else {
-                        echo "\nCouldn\'t connect to $mysqli->host_info\n";
-                    }
-
-                    $stmt = $mysqli->prepare($sql);
-                    $stmt->execute([$name, $phone, $email]);
-
-                    if (!$stmt->affected_rows) {
-                        throw new Exception('Couldn\'t INSERT into `tblClienti` !');
-                    }
-                    echo 'Inserted user: ', $name, ' ', $phone, ' ', $email;
-                    $stmt->close();
-                    $mysqli->close();
-                }
-
-            } catch (Exception $e) {
-                echo 'Caught exception: ', $e->getMessage(), "\n";
-                exit();
-            }
-        }
-    }
-    
     function DeleteClient()
     {
         if($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -178,89 +93,14 @@
         }
     }
     
-    function SearchClient()
-    {
-        if($_SERVER["REQUEST_METHOD"] == "POST") {
-            try {
-                mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); //Predefined Constants MySQLi
 
-                $sql = 'SELECT `numeClient`, `telefonClient`, `emailClient`, `pretComanda`, `adresaComanda`, `statusComanda`, `dataPlasareComanda` FROM `tblClienti` JOIN `tblComenzi`' .
-                    ' ON `idClient`=`codClient` WHERE `numeClient`=\'' . FormatName($_POST['search_client']) . '\' AND `telefonClient`=\'' . trim($_POST['search_phone']) . '\'';
 
-                //`numeClient`=\''.$_POST['search_client'].'\'
-                $mysqli = new mysqli('localhost', 'alex13dumi', 'steaua86.', 'magArtSportiveDB'); //OOP Style
 
-                if (!is_null($mysqli)) {
-                    echo 'Success.....' . $mysqli->host_info;
-                    echo '<br></br>';
-                    echo 'Connected !\nClient library version: ' . $mysqli->client_info;
-                    echo '<br ></br >';
-                    echo 'Server' . $mysqli->server_info;
-                } else {
-                    echo "\nCouldn\'t connect to $mysqli->host_info\n";
-                }
 
-                $result = $mysqli->query($sql);
-                if (!$result->num_rows)
-                    throw new Exception('Name or telephone doesn\'t exist');
-                else
-                {
-                    $i=0;
-                    while ($obj = $result->fetch_object())
-                    {
-		
-                        echo'
-                        <table class="table table-dark">
-                              <thead class="thead-dark">
-                                <tr>
-                                  <th scope="col">#</th>
-                                  <th scope="col">numeClient</th>
-                                  <th scope="col">telefonClient</th>
-                                  <th scope="col">emailClient</th>
-                                  <th scope="col">pretComanda</th>
-                                  <th scope="col">adresaComanda</th>
-                                  <th scope="col">statusComanda</th>
-                                  <th scope="col">dataPlasareComanda</th>
-                                </tr>
-                              </thead>
-                        ';
-                        echo'
-                              <tbody>
-                                <tr>
-                                  <th scope="row">' .$i. '</th>
-                                  <td>' .$obj->numeClient. '</td>
-                                  <td>' .$obj->telefonClient. '</td>
-                                  <td>' .$obj->emailClient. '</td>
-                                  <td>' .$obj->pretComanda. '</td>
-                                  <td>' .$obj->adresaComanda. '</td>
-                                  <td>' .$obj->statusComanda. '</td>
-                                  <td>' .$obj->dataPlasareComanda. '</td>
-                                </tr>
-                             </tbody>
-                            </table>
-                        ';
-                        $i++;
-                    }
-                }
-                $result->close();
-                $mysqli->close();
-            } catch (Exception $e) {
-                echo '<br></br>';
-                echo 'Caught exception: ' . $e->getMessage();
-                exit();
-            }
-        }
-    }
-    
-    if (isset($_POST['CreateClient'])){
-        CreateClient();
-    }
     if (isset($_POST['DeleteClient'])){
         DeleteClient();
     }
-    if(isset($_POST['SearchClient'])){
-        SearchClient();
-    }
+
 
 ?>
 
